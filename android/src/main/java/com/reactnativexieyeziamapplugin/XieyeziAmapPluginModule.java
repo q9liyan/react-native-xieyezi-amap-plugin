863f9500f7690df735f55eddf3e4af50ce052e90
xieyezi <1435398529@qq.com>
2021-12-03 13:19:40 +0800

    feat: add android modules

--- android/src/main/java/com/reactnativexieyeziamapplugin/XieyeziAmapPluginModule.java
+++ android/src/main/java/com/reactnativexieyeziamapplugin/XieyeziAmapPluginModule.java
@@ -1,19 +1,32 @@
 package com.reactnativexieyeziamapplugin;
 
-import androidx.annotation.NonNull;
 
+import androidx.annotation.NonNull;
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.module.annotations.ReactModule;
 
+import com.amap.api.location.AMapLocation;
+import com.amap.api.location.AMapLocationClient;
+import com.amap.api.location.AMapLocationClientOption;
+import com.amap.api.location.AMapLocationListener;
+import com.facebook.react.modules.core.DeviceEventManagerModule;
+
 @ReactModule(name = XieyeziAmapPluginModule.NAME)
 public class XieyeziAmapPluginModule extends ReactContextBaseJavaModule {
     public static final String NAME = "XieyeziAmapPlugin";
+    private final ReactApplicationContext context;
+
+    private AMapLocationClient client;
+    private AMapLocationClientOption option = new AMapLocationClientOption();
+
 
     public XieyeziAmapPluginModule(ReactApplicationContext reactContext) {
-        super(reactContext);
+
+      super(reactContext);
+      this.context = reactContext;
     }
 
     @Override
@@ -23,12 +36,37 @@ public class XieyeziAmapPluginModule extends ReactContextBaseJavaModule {
     }
 
 
-    // Example method
-    // See https://reactnative.dev/docs/native-modules-android
     @ReactMethod
-    public void multiply(int a, int b, Promise promise) {
-        promise.resolve(a * b);
+    public  void init(String key, Promise promise) {
+      if(client != null) {
+        client.onDestroy();
+      }
+      AMapLocationClient.setApiKey(key);
+      client = new AMapLocationClient(context);
+      promise.resolve(null);
     }
 
-    public static native int nativeMultiply(int a, int b);
+
+    @ReactMethod
+    public  void start() {
+      client.startLocation();
+    }
+
+    @ReactMethod
+    public void stop() {
+      client.stopLocation();
+    }
+
+    @ReactMethod
+    public void isStarted(Promise promise) {
+      promise.resolve(client.isStarted());
+    }
+
+    @ReactMethod
+    public void setLocationMode(String mode) {
+      option.setLocationMode(AMapLocationClientOption.AMapLocationMode.valueOf(mode));
+      client.setLocationOption(option);
+    }
+
+
 }
